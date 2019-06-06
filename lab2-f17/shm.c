55d64bbff215433cebae4c764cdfd68b0d4ce9d5
ptr2578 <ptr2578@github.com>
2019-06-05 19:52:02 -0700

    4 done

--- lab2-f17/shm.c
+++ lab2-f17/shm.c
@@ -32,18 +32,57 @@ int shm_open(int id, char **pointer) {
 
 //you write this
 
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  struct proc *curproc = myproc();
+  uint sz = PGROUNDUP(curproc->sz);
+  int i;
+  int j = 64;
+  int k = 0;
+  acquire(&(shm_table.lock));
+  for (i = 0; i< 64; i++) {
+    if (j == 64 && shm_table.shm_pages[i].id == 0) {
+      j = i;
+    }
+    if (shm_table.shm_pages[i].id == id) {
+      k = 1;
+      mappages(curproc->pgdir, (char *)sz, PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+      shm_table.shm_pages[i].refcnt++;
+      break;
+    }
+  }
+  
+  if (k == 0 && j < 64) {
+    shm_table.shm_pages[j].id = id;
+    shm_table.shm_pages[j].frame = kalloc();
+    memset(shm_table.shm_pages[j].frame, 0, PGSIZE);
+    mappages(curproc->pgdir, (char *)sz, PGSIZE, V2P(shm_table.shm_pages[j].frame), PTE_W|PTE_U);
+    shm_table.shm_pages[j].refcnt++;
+  }
+  
+  curproc->sz = sz + PGSIZE;
+  *pointer = (char *)sz;
+  release(&(shm_table.lock));
+  
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
 //you write this too!
-
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  int i;
+  acquire(&(shm_table.lock));
+  for (i = 0; i< 64; i++) {
+    if (shm_table.shm_pages[i].id == id) {
+      if (shm_table.shm_pages[i].refcnt > 0) {
+        shm_table.shm_pages[i].refcnt--;
+      }
+      if (shm_table.shm_pages[i].refcnt == 0) {
+        shm_table.shm_pages[i].id =0;
+        shm_table.shm_pages[i].frame =0;
+        break;
+      }
+    }
+  }
+  release(&(shm_table.lock));
+  
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
